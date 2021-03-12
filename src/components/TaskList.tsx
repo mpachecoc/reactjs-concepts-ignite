639e8da968f2a551989bbe83baf26d140f2d5c27
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2021-03-12 17:13:24 -0400

    Add Task addition, deletion and completion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,41 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      alert('You must write a title!')
+      return
+    }
+
+    const newTask = {
+      id: Math.floor(Math.random() * Math.floor(10000)),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    setTasks([...tasks, newTask])
+
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        }
+      }
+
+      return task
+    })
+
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksUpdated = tasks.filter(task => task.id !== id)
+
+    setTasks(tasksUpdated)
   }
 
   return (
